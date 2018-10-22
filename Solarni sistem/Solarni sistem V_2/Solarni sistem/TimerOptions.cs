ca6c84bce04471cb799243c49eecb6e79942437c
Filippenpen <44364403+Filippenpen@users.noreply.github.com>
2018-10-23 00:33:19 +0200

    Initial commit

---
+++ Solarni sistem/Solarni sistem V_2/Solarni sistem/TimerOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+    class TimerOptions
+    {
+        
+        public static bool TimerOnOrOff = false;
+        public static double t = 20000;//dan
+    }
+}
